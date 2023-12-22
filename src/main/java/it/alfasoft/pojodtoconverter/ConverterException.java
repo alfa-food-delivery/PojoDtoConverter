abe2cc2c9c26a9013c4c3be127c046ac221c08c3
Zambo1998 <alessandrobrillante78@gmail.com>
2023-12-22 10:27:30 +0100

    fix pojo

--- src/main/java/it/alfasoft/pojodtoconverter/ConverterException.java
+++ src/main/java/it/alfasoft/pojodtoconverter/ConverterException.java
@@ -3,6 +3,5 @@ package it.alfasoft.pojodtoconverter;
 public class ConverterException extends Exception {
     public ConverterException(){ super(); }
     public ConverterException(String message) { super(message); }
-
     public ConverterException(Exception e){ super(e); }
 }
--- src/main/java/it/alfasoft/pojodtoconverter/OffuscamentoBidirezionale.java
+++ src/main/java/it/alfasoft/pojodtoconverter/OffuscamentoBidirezionale.java
@@ -28,4 +28,5 @@ public class OffuscamentoBidirezionale {
 
         return Integer.parseInt(decryptedString);
     }
+
 }
